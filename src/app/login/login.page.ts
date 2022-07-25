b7828cee145905da646953c35019e2e1b6a960b3
Antony <72855310@continental.edu.pe>
2022-07-25 00:10:17 -0500

    Tarea clase 07/08/2022

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+  validation_messages = {
+    email: [
+      { type: "required", message: "El email es obligatorio"},
+      { type: "pattern", message: "El email no es valido" }
+    ],
+    password: [
+      { type: "required", message: "El password es obligatorio" },
+      { type: "minlength", message: "El password no es valido" }
+    ]
+  }
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-]+$")
+        ])
+      ),
+      password: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(6)
+        ])
+      )
+    })
+   }
+
+  ngOnInit() {
+  }
+
+}
